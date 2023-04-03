88ed5e6ae8b3acccab1e620fd174f09c986b7826
unknown <wilhemh38@hotmail.fr>
2023-04-03 20:05:39 +0200

    update

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,9 @@
 function PokemonCard() {
     return <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbisare"/>
-<figcaption>bulbasaur</figcaption> 
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"alt="Bulbisare"/>
+        <figcaption>bulbasaur</figcaption> 
 
-</figure>
+    </figure>
 }
 
 export default PokemonCard
