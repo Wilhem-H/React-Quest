02027bd71e3f419e6c865740a793ed63d633c289
unknown <wilhemh38@hotmail.fr>
2023-04-04 11:14:03 +0200

    React basis 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,32 @@
-function PokemonCard() {
-    return <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"alt="Bulbisare"/>
-        <figcaption>bulbasaur</figcaption> 
+// function PokemonCard() {
+//     return <figure> 
+//         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"alt="Bulbisare"/>
+//         <figcaption>bulbasaur</figcaption> 
 
-    </figure>
+//     </figure>
+// }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
+  function PokemonCard() {
+    const pokemon = pokemonList[1]
+        return <figure> 
+                {pokemon.imgSrc? <img src= {pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
+                <figcaption>{pokemon.name}</figcaption> 
+                </figure>
+    
 }
+
 
 export default PokemonCard
