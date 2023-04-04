0923c7ce76f78d40307869b59087c9939c0da206
unknown <wilhemh38@hotmail.fr>
2023-04-04 19:13:47 +0200

    React basis 4

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,25 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}/>
     </div>
   )
 }
+
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,25 +6,17 @@
 //     </figure>
 // }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
 
 
-  function PokemonCard() {
-    const pokemon = pokemonList[1]
+
+  function PokemonCard(props) {
+    console.log(props)
+    
         return <figure> 
-                {pokemon.imgSrc? <img src= {pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
-                <figcaption>{pokemon.name}</figcaption> 
-                </figure>
+                {props.imgSrc? <img src= {props.imgSrc} alt={props.name}/> : <p>???</p>}
+                <figcaption>{props.name}</figcaption> 
+            </figure>
     
 }
 
